c5d684b6ca9c48fb1d491fe92bf6b36598351c96
tiger <duongvuthai2018@gmail.com>
2022-11-13 21:24:03 +0700

    homework

---
+++ homework.js
@@ -0,0 +1,24 @@
+// bài 1
+// function titleCase(string) {
+//     let a = string.toLowerCase().split(" ");
+//     for(var i = 0; i< a.length; i++){
+//         a[i] = a[i][0].toUpperCase() + a[i].slice(1);
+//     }
+//     return a.join(" ");
+//     }
+// let c = titleCase('vu phong thai');
+// console.log(c)
+// bài 2
+// let a = '       Doraemon and Dorami are brother              '
+// console.log(a.replace(/a/g,"4").replace(/e/g,"3").replace(/i/g,"1").replace(/o/g,"0").replace(/s/g,"5").trimEnd().trimStart())
+// bài 3
+// function checkString(string) {
+//     if (string.length >= 3 && !string.includes('ing')) {
+//     console.log(`${string}ing`);
+//     } else if (string.includes('ing')) {
+//     console.log(`${string}ly`);
+//     } else if (string.length < 3) {
+//     console.log(string);
+//     }
+//     }
+//     checkString('swiming');
